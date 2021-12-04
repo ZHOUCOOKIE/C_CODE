a7f181e44965a0b6e9e0e2c7dffe720afce17231
ZHOUIU <zhoucookie@outlook.com>
2021-12-04 23:31:44 +0800

    更新了一下代码

---
+++ 大学计算机课/大学计算机课/方法2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//static修饰全局变量
+//static int a = 2020;
+//
+// static修饰函数
+//void print(int x)
+//{
+//	printf("%d", x);
+//}
+//int add(int x, int y)
+//{
+//	return x + y;
+//}
